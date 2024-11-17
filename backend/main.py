03f6aab6baa86b22b4903d8511007ca7dff2ff21
Sanjay R <112553576+sanjay-29-29@users.noreply.github.com>
2024-11-17 22:14:55 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,38 +3,60 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import pandas as pd
 
-model = joblib.load("car_price_model.h5")
+model = joblib.load("car_price_model.pkl")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 class CarFeatures(BaseModel):
-    make: str
-    model: str
-    year: int
-    fuel_type: str
-    transmission: str
-    driven_wheels: str
-    engine_hp: float
-    engine_cylinders: int
-    highway_mpg: int
+    Make: str
+    Model: str
+    Engine_Fuel_Type: str
+    Transmission_Type: str
+    Driven_Wheels: str
+    Market_Category: str
+    Vehicle_Size: str
+    Vehicle_Style: str
+    Year: int
+    Engine_HP: float
+    Engine_Cylinders: float
+    Number_of_Doors: float
+    highway_MPG: int
     city_mpg: int
+    Popularity: int
 
 @app.post("/predict")
 async def predict_price(features: CarFeatures):
-    input_data = np.array([[
-        features.make, features.model, features.year, features.fuel_type,
-        features.transmission, features.driven_wheels, features.engine_hp,
-        features.engine_cylinders, features.highway_mpg, features.city_mpg
-    ]])
+    try:
+        input_data = pd.DataFrame([{
+            "Make": features.Make,
+            "Model": features.Model,
+            "Engine Fuel Type": features.Engine_Fuel_Type,
+            "Transmission Type": features.Transmission_Type,
+            "Driven_Wheels": features.Driven_Wheels,
+            "Market Category": features.Market_Category,
+            "Vehicle Size": features.Vehicle_Size,
+            "Vehicle Style": features.Vehicle_Style,
+            "Year": features.Year,
+            "Engine HP": features.Engine_HP,
+            "Engine Cylinders": features.Engine_Cylinders,
+            "Number of Doors": features.Number_of_Doors,
+            "highway MPG": features.highway_MPG,
+            "city mpg": features.city_mpg,
+            "Popularity": features.Popularity
+        }])
 
-    prediction = model.predict(input_data)[0]
-    return {"predicted_price": round(prediction, 2)}
+        prediction = model.predict(input_data)[0]
+
+        return {"predicted_price": round(prediction, 2)}
+    except Exception as e:
+        return {"error": str(e)}
